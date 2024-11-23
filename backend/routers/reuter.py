93d43a1b23a399d4946fe8b5d0a3a8ec473acf4e
crsPerzGonzal <Cperezg22@ul.edu.co>
2024-11-23 15:25:25 -0500

    Update reuter.py

--- backend/routers/reuter.py
+++ backend/routers/reuter.py
@@ -3,7 +3,7 @@ from fastapi import Depends , HTTPException
 from backend import Crud as crud
 from backend.core.confi import get_db
 from sqlalchemy.orm import Session
-from backend.schemas.response import UserSchema, Response, RegiSchema
+from backend.schemas.response import UserSchema, Response, RegiSchema, ProductSchema, OrderSchema
 
 router = APIRouter()
 
@@ -26,22 +26,34 @@ async def create_user_name(request:UserSchema, db:Session = Depends(get_db)):
 
 @router.post('/create')
 async def create_regi(request:RegiSchema, db:Session = Depends(get_db)):
+    crud.create_user(db, RegiUser = request)
+    print(request)
+    return Response(status="ok",
+                    code="200",
+                    message="creado registro", result=request.dict(exclude_none=True))
+
+
+@router.get('/producto', response_model=list[ProductSchema])
+async def mostrar_producto( db: Session = Depends(get_db)):
     try:
-        # Llama a la función CRUD para crear el usuario
-        new_usuario = create_regi(db, regiUser=request)
-        return {
-            "status": "ok",
-            "code": 200,
-            "message": "Usuario creado exitosamente",
-            "result": {
-                "id": new_usuario.id,
-                "username": new_usuario.username,
-                "email": new_usuario.email
-            }
-        }
-    except HTTPException as http_err:
-        # Reenvía los errores definidos
-        raise http_err
-    except Exception as err:
-        # Captura errores inesperados
-        raise HTTPException(status_code=500, detail=f"Error interno: {err}")
+        productos = crud.mostrar_product(db)
+        return productos
+    except ValueError as ve:
+        raise HTTPException(status_code=404, detail=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al mostrar el producto: {e}")
+    
+
+
+
+@router.post("/inset_orden")
+async def insert_orden(request: OrderSchema, db: Session = Depends(get_db)):
+    try:
+        crud.insert_orden(db, orderResponse  = request)
+        return Response(status="ok",
+                    code="200",
+                    message="insertar orden", result=request.dict(exclude_none=True))
+    except ValueError as ve:
+        raise HTTPException(status_code=404, detail=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"error de insertar pedido: {e}")
